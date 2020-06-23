c6049996b06eb7f410c1772fa6ad362e21bd3276
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-06-23 20:00:51 +0100

    Initial commit

---
+++ tutorial_app/src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Header from "./components/layouts/Header";
+import Footer from "./components/layouts/Footer";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
