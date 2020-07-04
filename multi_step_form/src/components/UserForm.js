57fd58a6db1bd279631f99b7b786416022b6fc90
Amy Jade Richards <amyjaderichards@googlemail.com>
2020-07-04 13:57:43 +0100

    Initial commit

---
+++ multi_step_form/src/components/UserForm.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+export class UserForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+
+        )
+    }
+}
+
+export default UserForm
